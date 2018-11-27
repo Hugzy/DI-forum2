ad7064152e4d41569306a7099a19b82d6ed9f55f
Hugzy <frede.madsen3@gmail.com>
2018-11-27 15:31:47 +0100

    first working graphql request from vue

--- server/thread.js
+++
@@ -1,2 +0,0 @@
-var { fakeDatabase } = require("./database")
-
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>Example 1</h3>
+    <div>
+      Data: {{ example1 }}
+    </div>
+    <button @click="getLanguage">Get Language</button>
+    <hr> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: 'app',
+    data() {
+        return {
+            example1: ''
+        };
+    },
+    methods: {
+        async getLanguage() {
+            try {
+                const res = await axios.post('http://localhost:4000/graphql', {
+                    query:
+                        '{ forums(id: "7d95f170-a066-4824-b4f6-8b94ddd92276"){name} }'
+                });
+                this.example1 = res.data.data.forums[0].name;
+            } catch (e) {
+                console.log('err', e);
+            }
+        }
+    }
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
